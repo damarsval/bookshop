4acb0a06daa902111d123805a1eb85d31e6345a6
damarsval <camarik032@gmail.com>
2025-02-02 17:11:21 +0300

    mainpage

--- js/books.js
+++ js/books.js
@@ -134,99 +134,276 @@ const books = [
     discount: "- 29%", 
     title: "Вегетация",
     author: "Алексей Иванов" 
-  }, {
-    id: "BK-I8SHID", 
-    image: "images/cafe.webp",
-    oldPrice: "436 &#8381;",
-    newPrice: "314 &#8381;",
-    discount: "- 28%", 
-    title: "Кафе на краю земли",
-    author: "Джон Стрелеки" 
-  }, { 
-    id: "BK-09YPOZ",
-    image: "images/wildrobot.webp",
-    oldPrice: "978 &#8381;",
-    newPrice: "792 &#8381;",
-    discount: "- 19%", 
-    title: "Миссия дикого робота",
-    author: "Питер Браун" 
-  }
+  }, 
 ];
 
 const books2 = [
   { 
-    id: "BK-ZY8J7G",
-    image: "images/notesfromundeg.jpg",
-    oldPrice: "530 &#8381;",
-    newPrice: "420 &#8381;",
-    discount: "- 15%", 
-    title: "The Haunted Hotel",
-    author: "Wilkie Collins" 
-  }, {
-    id: "BK-OPLGSJ", 
-    image: "images/osamu.webp",
-    oldPrice: "330 &#8381;",
-    newPrice: "264 &#8381;",
-    discount: "- 20%", 
-    title: "Человек недостойный",
-    author: "Осаму Дадзай" 
-  }, {
-    id: "BK-6S6DDD", 
-    image: "images/hawking.webp",
-    oldPrice: "450 &#8381;",
-    newPrice: "315 &#8381;",
-    discount: "- 30%", 
-    title: "Теория всего",
-    author: "Стивен Хокинг" 
-  }, { 
-    id: "BK-04JJZZ",
-    image: "images/carr.webp",
-    oldPrice: "1100 &#8381;",
-    newPrice: "935 &#8381;",
-    discount: "- 15%", 
-    title: "Слепой цирюльник",
-    author: "Джон Карр" 
-  }, { 
-    id: "BK-TZ4UAK",
-    image: "images/goldcow.webp",
-    oldPrice: "620 &#8381;",
-    newPrice: "434 &#8381;",
-    discount: "- 30%", 
-    title: "Золотой теленок",
-    author: "Илья Ильф, Евгений Петров" 
-  }, { 
-    id: "BK-L5F3CG",
-    image: "images/fudz.webp",
-    oldPrice: "1020 &#8381;",
-    newPrice: "856 &#8381;",
+    id: "BK-Z78JIG",
+    image: "images/bracelet.webp",
+    oldPrice: "359 &#8381;",
+    newPrice: "284 &#8381;",
     discount: "- 21%", 
-    title: "Оглянись",
-    author: "Фудзимото Тацуки" 
+    title: "Гранатовый браслет",
+    author: "Александр Куприн" 
+  }, {
+    id: "BK-OPLISJ", 
+    image: "images/crime.webp",
+    oldPrice: "388 &#8381;",
+    newPrice: "296 &#8381;",
+    discount: "- 24%", 
+    title: "Преступление и наказание",
+    author: "Федор Достоевский" 
+  }, {
+    id: "BK-6L8NID", 
+    image: "images/dno.webp",
+    oldPrice: "359 &#8381;",
+    newPrice: "284 &#8381;",
+    discount: "- 21%", 
+    title: "На дне",
+    author: "Максим Горький" 
+  }, { 
+    id: "BK-04MYXZ",
+    image: "images/fathers.webp",
+    oldPrice: "329 &#8381;",
+    newPrice: "278 &#8381;",
+    discount: "- 16%", 
+    title: "Отцы и дети",
+    author: "Иван Тургенев" 
+  }, { 
+    id: "BK-IJHZHF",
+    image: "images/faust.webp",
+    oldPrice: "388 &#8381;",
+    newPrice: "296 &#8381;",
+    discount: "- 24%", 
+    title: "Фауст",
+    author: "И. В. Гёте" 
+  }, { 
+    id: "BK-YGZ4SZ",
+    image: "images/gore.webp",
+    oldPrice: "359 &#8381;",
+    newPrice: "284 &#8381;",
+    discount: "- 21%", 
+    title: "Горе от ума",
+    author: "Александр Грибоедов" 
   },
 
-
+ { id: "BK-ZC8J7G",
+  image: "images/jane.webp",
+  oldPrice: "388 &#8381;",
+  newPrice: "296 &#8381;",
+  discount: "- 24%", 
+  title: "Джейн Эйр",
+  author: "Шарлотта Бронте" 
+}, {
+  id: "BK-OPUPZJ", 
+  image: "images/lord.webp",
+  oldPrice: "447 &#8381;",
+  newPrice: "341 &#8381;",
+  discount: "- 24%", 
+  title: "Повелитель мух",
+  author: "Уильям Голдинг" 
+}, {
+  id: "BK-X4YGAB", 
+  image: "images/martin.webp",
+  oldPrice: "376 &#8381;",
+  newPrice: "287 &#8381;",
+  discount: "- 24%", 
+  title: "Мартин Иден",
+  author: "Джек Лондон" 
+}, { 
+  id: "BK-VZXCQ6",
+  image: "images/mouse.webp",
+  oldPrice: "435 &#8381;",
+  newPrice: "332 &#8381;",
+  discount: "- 24%", 
+  title: "О мышах и людях",
+  author: "Джон Стейнбек" 
+},
+{ 
+  id: "BK-04JJZZ",
+  image: "images/portrait.webp",
+  oldPrice: "329 &#8381;",
+  newPrice: "278 &#8381;",
+  discount: "- 16%", 
+  title: "Портрет Дориана Грея",
+  author: "Оскар Уайльд" 
+}, { 
+  id: "BK-CG3YAK",
+  image: "images/pride.webp",
+  oldPrice: "365 &#8381;",
+  newPrice: "299 &#8381;",
+  discount: "- 18%", 
+  title: "Гордость и предубеждение",
+  author: "Джейн Остин" 
+}, { 
+  id: "BK-L5IHCG",
+  image: "images/prince.webp",
+  oldPrice: "329 &#8381;",
+  newPrice: "278 &#8381;",
+  discount: "- 16%", 
+  title: "Маленький принц",
+  author: "Антуан де Сент-Экзюпери" 
+},
+{ 
+  id: "BK-E6GLSR",
+  image: "images/steppenwolf.webp",
+  oldPrice: "801 &#8381;",
+  newPrice: "611 &#8381;",
+  discount: "- 15%", 
+  title: "Степной волк",
+  author: "Герман Гессе" 
+}, { 
+  id: "BK-K7NUAK",
+  image: "images/us.webp",
+  oldPrice: "359 &#8381;",
+  newPrice: "284 &#8381;",
+  discount: "- 21%", 
+  title: "Мы",
+  author: "Евгений Замятин" 
+}, { 
+  id: "BK-B9BLSN",
+  image: "images/gates.webp",
+  oldPrice: "447 &#8381;",
+  newPrice: "341 &#8381;",
+  discount: "- 24%", 
+  title: "Ворота Расёмон",
+  author: "Рюноскэ Акутагава" 
+}
 ];
 
-
-/*products.forEach((product) => {
-  const productClone = productTemplate.cloneNode(true);
-  productClone.querySelector('.product-card__img').src = product.image;
-  productClone.querySelector('.product-info__title').textContent = product.title;
-  productClone.querySelector('.product-info__author').textContent = product.author;
-  productClone.querySelector('.product-price__old').textContent = product.oldPrice;
-  productClone.querySelector('.product-price__new').textContent = product.newPrice;
-  productClone.querySelector('.product-price__discount').textContent = product.discount;
-
-  
-  swiperWrapper.appendChild(productClone);
-});
-*/
-
-/*var swiper = new Swiper('.swiper-container', {
-  loop: true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+const books3 = [
+  { 
+    id: "BK-XZ8JIG",
+    image: "images/humilated.webp",
+    oldPrice: "329 &#8381;",
+    newPrice: "278 &#8381;",
+    discount: "- 16%", 
+    title: "Униженные и оскорбленные",
+    author: "Федор Достоевский" 
+  }, {
+    id: "BK-7U5HSJ", 
+    image: "images/blindspot.webp",
+    oldPrice: "1 297 &#8381;",
+    newPrice: "989 &#8381;",
+    discount: "- 24%", 
+    title: "Ложная слепота",
+    author: "Питер Уоттс" 
+  }, {
+    id: "BK-R5IHLD", 
+    image: "images/usualstory.webp",
+    oldPrice: "388 &#8381;",
+    newPrice: "296 &#8381;",
+    discount: "- 24%", 
+    title: "Обыкновенная история",
+    author: "Иван Гончаров" 
+  }, { 
+    id: "BK-VН3YXZ",
+    image: "images/odyssey.webp",
+    oldPrice: "1 415 &#8381;",
+    newPrice: "1 079 &#8381;",
+    discount: "- 24%", 
+    title: "Илиада. Одиссея",
+    author: "Гомер" 
+  }, { 
+    id: "BK-R5ZASQ",
+    image: "images/timemachine.webp",
+    oldPrice: "424 &#8381;",
+    newPrice: "323 &#8381;",
+    discount: "- 24%", 
+    title: "Машина времени. Остров доктора Моро",
+    author: "Герберт Уэллс" 
+  }, { 
+    id: "BK-HY8CZQ",
+    image: "images/ctulhu.webp",
+    oldPrice: "919 &#8381;",
+    newPrice: "701 &#8381;",
+    discount: "- 24%", 
+    title: "Зов Ктулху",
+    author: "Говард Филлипс Лавкрафт" 
   },
-});*/
+
+ { id: "BK-T5ZAGQ",
+  image: "images/chehov.webp",
+  oldPrice: "860 &#8381;",
+  newPrice: "656 &#8381;",
+  discount: "- 24%", 
+  title: "Малое собрание сочинений",
+  author: "Антон Чехов" 
+}, {
+  id: "BK-OYJPZJ", 
+  image: "images/android.webp",
+  oldPrice: "801 &#8381;",
+  newPrice: "611 &#8381;",
+  discount: "- 24%", 
+  title: "Мечтают ли андроиды об электроовцах?",
+  author: "Филип Киндред Дик" 
+}, {
+  id: "BK-S1JIAK", 
+  image: "images/firstlaw.webp",
+  oldPrice: "3 067 &#8381;",
+  newPrice: "2 339 &#8381;",
+  discount: "- 24%", 
+  title: "Первый закон. Трилогия",
+  author: "Джо Аберкромби" 
+}, { 
+  id: "BK-NC5LT6",
+  image: "images/neuromancer.webp",
+  oldPrice: "1 651 &#8381;",
+  newPrice: "1 259 &#8381;",
+  discount: "- 24%", 
+  title: `Нейромант. Трилогия "Киберпространство"`,
+  author: "Уильям Гибсон" 
+},
+{ 
+  id: "BK-F6KHFC",
+  image: "images/temple.webp",
+  oldPrice: "365 &#8381;",
+  newPrice: "278 &#8381;",
+  discount: "- 16%", 
+  title: "Золотой Храм",
+  author: "Юкио Мисима" 
+}, { 
+  id: "BK-T2XZWJ",
+  image: "images/player.webp",
+  oldPrice: "359 &#8381;",
+  newPrice: "284 &#8381;",
+  discount: "- 21%", 
+  title: "Игрок",
+  author: "Федор Достоевский" 
+}, { 
+  id: "BK-H4VLNG",
+  image: "images/letters.webp",
+  oldPrice: "359 &#8381;",
+  newPrice: "284 &#8381;",
+  discount: "- 21%", 
+  title: "Маленький принц",
+  author: "Антуан де Сент-Экзюпери" 
+},
+{ 
+  id: "BK-E6GLSR",
+  image: "images/demian.webp",
+  oldPrice: "424 &#8381;",
+  newPrice: "323 &#8381;",
+  discount: "- 15%", 
+  title: "Демиан",
+  author: "Герман Гессе" 
+}, { 
+  id: "BK-Y7LVSK",
+  image: "images/monte.webp",
+  oldPrice: "412 &#8381;",
+  newPrice: "314 &#8381;",
+  discount: "- 24%", 
+  title: "Граф Монте-Кристо. Том 1",
+  author: "Александр Дюма (отец)" 
+}, { 
+  id: "BK-Y8LVSK",
+  image: "images/monte-2.webp",
+  oldPrice: "412 &#8381;",
+  newPrice: "314 &#8381;",
+  discount: "- 24%", 
+  title: "Граф Монте-Кристо. Том 2",
+  author: "Александр Дюма (отец)" 
+},
+];
+
--- js/mySwiper.js
+++ js/mySwiper.js
@@ -2,23 +2,30 @@ document.addEventListener("DOMContentLoaded", () => {
   // Инициализация Swiper для каждого слайдера
   const swiperContainers = document.querySelectorAll(".swiper-container");
   swiperContainers.forEach((container, index) => {
-    const swiperId = container.getAttribute("data-id"); // Получаем уникальный ID слайдера
+    const swiperId = container.getAttribute("data-id");
 
     const nextBtn = container.querySelector(`.swiper-button-next`);
     const prevBtn = container.querySelector(`.swiper-button-prev`);
     new Swiper(container, {
-      loop: true, // Бесконечная прокрутка
+      loop: true, 
       navigation: {
-        nextEl: nextBtn, // Кнопка "Вперёд"
-        prevEl: prevBtn, // Кнопка "Назад"
+        nextEl: nextBtn, 
+        prevEl: prevBtn, 
       },
-      slidesPerView: 1, // Количество слайдов, отображаемых одновременно
-      spaceBetween: 30, // Расстояние между слайдами
+      slidesPerView: 1,
+      spaceBetween: 10,
       breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 50
+        },
         1200: {
-          slidesPerView: 1,
-          spaceBetween: 1500,
-          slideToClickedSlide: true,
+          slidesPerView: 6,
+          slidesPerGroup: 4
         }
       }
     });
